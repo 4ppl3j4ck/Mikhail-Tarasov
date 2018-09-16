46784f6ae77892a731f05fdf28dbf761ce3cfd33
4ppl3j4ck <43236331+4ppl3j4ck@users.noreply.github.com>
2018-09-16 14:39:33 +0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "name: Mikhail Tarasov\naddress: the city of Dzerzhinsk, Chkalov Avenue, house 13, apartment 9" << endl;
+    return 0;
+}
---
+++ ex2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int farlong;
+   int x;
+   farlong = 220;
+   cout<<"Enter the distance in furlongs and convert it to yards:";
+   cout << endl;
+   cin >> x;
+   cout << x <<" farlong = " << farlong * x <<" yard" ;
+   return 0;
+}
